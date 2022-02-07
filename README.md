1fa9266f351395b1fdee734cb6db6f10c7aae9e1
Nick O'Leary <nick.oleary@gmail.com>
2022-02-07 22:43:15 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# FlowForge Telemetry Ping App
+
+This repo contains the code for the FlowForge Telemetry Ping App. This
+is a very simple HTTPS end point that is used to collect usage information
+from instances of FlowForge that have opted-in to sharing the information.
+
+The received data stored in a DynanoDB table.
+
+This service does not store any identifying information that can be used
+to identify individual users. The data is augmented with a hash of the sending IP address so that separate requests can be correlated - but the
+actual IP address is not stored.
+
+## Requirements
+
+* [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html) installed and configured
+* [AWS Serverless Application Model](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html) (AWS SAM) installed
---
+++ src/app.js
@@ -0,0 +1,63 @@
+const AWS = require('aws-sdk')
+// const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'})
+const client = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+const crypto = require('crypto')
+const sha256 = value => crypto.createHash('sha256').update(value).digest().toString('base64')
+
+
+exports.handler = async (event, context) => {
+    try {
+        if (event.body) {
+            const payload = JSON.parse(event.body)
+
+            if (!payload.instanceId) {
+                throw new Error("Invalid request")
+            }
+
+            let callingIP = "unknown"
+            if (event.requestContext && event.requestContext.http)  {
+                callingIP =  sha256(event.requestContext.http.sourceIp)
+            }
+            const params = {
+                TableName:'flowforge-ping-data',
+                Item: {
+                    instanceId: payload.instanceId || "unknown",
+                    createdAt: (new Date()).toISOString(),
+                    ip: callingIP,
+                    env: payload.env,
+                    platform: payload.platform
+                }
+            }
+
+            var msg;
+            try{
+                await client.put(params).promise();
+                msg = 'okay';
+            } catch(err){
+                msg = err;
+            }
+            var response = {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    status: msg
+                })
+            }
+
+            // instanceId: <instanceId>
+            // ip: <hash of calling IP>
+            // env:
+            //    nodejs: `process.version`
+            //    flowforge: <version>
+            //    os:
+            //      type: `os.type()`
+            //      release: `os.release()`
+            //      arch:  `os.arch()`
+            //
+        }
+    } catch(err) {
+        console.error(err)
+        return err
+    }
+
+    return response
+}
